0f4224b7f832ffb31dac53a6163f41da29194384
kaeriv93 <vsan93@yahoo.com>
2022-06-06 11:38:12 -0500

    created register route api

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from .views import UserDetailAPI, RegisterUserAPIView
 from . import views
 
 router = routers.DefaultRouter()
@@ -8,5 +9,7 @@ router.register(r'blog', views.BlogViewSet)
 
 urlpatterns =[
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
+    path('get-details', UserDetailAPI.as_view()),
+    path('register', RegisterUserAPIView.as_view()),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import CustomUserSerializer, BlogSerializer
+from .serializers import CustomUserSerializer, BlogSerializer, RegisterSerializer
 from .models import CustomUser, Blog
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
+
+
+class UserDetailAPI(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (AllowAny,)
+    def get(self,request,*args,**kwargs):
+        user = CustomUser.objects.get(id=request.user.id)
+        serializer = CustomUserSerializer(CustomUser)
+        return Response(serializer.data)
+
+class RegisterUserAPIView(generics.CreateAPIView):
+  permission_classes = (AllowAny,)
+  serializer_class = RegisterSerializer
 
 class CustomerUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('first_name')
